3aa558a733d5708cd16e3379a75175235c89ab46
Chocolate <artemnes1j@gmail.com>
2025-04-18 21:38:59 +0300

    documentation main page added

--- include/hashtab.h
+++ include/hashtab.h
@@ -1,5 +1,60 @@
+/**
+* @mainpage         Hashtab
+
+* @section          Introduction
+Implementation of hashtab with 4.14 times optimization with assembly inlines,
+assembly code and intrinsics for x86-64 processors with SSE 4.2.
+
+* @section          Features
+- Can be compiled for every processor with optimization turned off.
+- Can store custom data, but only one per build. See data_t.
+
+* @section          Usage
+\code{.cpp}
+#include <library.h>
+
+const char key1[32] = {};
+const char key2[32] = {};
+
+int main(int argc, const char *argv[]) {
+    // Writing keyword to 32 bytes arrays.
+    snprintf(key1, 32, "First keyword");
+    snprintf(key2, 32, "Second keyword");
+
+    // Calling hashtab constructor
+    hashtab_t hashtab = {};
+    hashtab_ctor(&hashtab);
+
+    // Inserting elements in hashtab
+    data_t data = {.value = 10};
+    hashtab_insert(&hashtab, key1, data);
+    hashtab_insert(&hashtab, key2, NULL);
+
+    // Searching element in hashtab
+    data_t *result = NULL;
+    hashtab_search(&hashtab, "First keyword", &result);
+    printf("%d", result->value);
+
+    // Removing element from hashtab
+    hashtab_remove(&hashtab, "Second keyword");
+
+    // Calling hashtab destructor
+    hashtab_dtor(&hashtab);
+    return 0;
+}
+\endcode
+
+* @section          Database format
+- Every keyword in database file has to be stored in blocks of 32 bytes.
+- Every symbol of 32 bytes is treated as a part of keyword.
+- It is expected that database size is divisible by 32.
+
+* @section          Links
+- [Репозиторий GitHub](https://github.com/artemneskorodov/hashtab)
+*/
 #ifndef HASHTAB_H
 #define HASHTAB_H
+/*============================================================================*/
 /*============================================================================*/
 /**
 * @file     hashtab.h
